746df7fa22381f99f6ba965e310e4ca705ce1a0a
eng-oday <odaydieg@gmail.com>
2023-09-21 12:59:52 +0300

    Initial Commit

---
+++ Clean_MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clean_MVVM
+//
+//  Created by 3rabApp-oday on 21/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
